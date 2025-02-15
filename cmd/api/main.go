9c88545b5c5049faf07d80085130ef9d2e424cc1
Christian A. Cardenas <cardenasc33@gmail.com>
2025-02-14 22:36:18 -0600

    Added handlers for API endpoint setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,11 +5,26 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi" //package for web dev
-	"github.com/cardenasc33/goapi/internal/handlers" // importing our own package
+	// "ssh://git@github.com/cardenasc33/goapi/internal/handlers" // importing our own package
 	log "github.com/sirupsen/logrus" // used to log errors for debugging
 
 )
 
 func main () {
+	log.SetReportCaller(true) // When printing something out, you get the file and line number
 	
+	// Returns a pointer to a MUX type
+	var r *chi.Mux = chi.NewRouter()  //struct to set up API
+	handlers.Handler(r)  // Set up router, i.e add enpoint definitions
+
+	fmt.Println("Starting Go API service...")
+
+	fmt.Println("GO API")
+
+	// Start server
+	// @params: (base location of server, handler that Mux type satisfies)
+	err := http.ListenAndServe("localhost:8000", r)
+	if err != nil {
+		log.Error(err)  // log any errors when starting the server
+	}
 }
---
+++ internal/handlers/api.go
@@ -0,0 +1,7 @@
+package handlers
+
+import (
+	"github.com/go-chi/chi"
+	chimiddle "github.com/go-chi/chi/middleware"
+	"github.com/cardenasc33/goapi/internal/middleware"
+)
